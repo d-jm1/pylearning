482dd2f741836ec072c9c2f1fbef07a12b6fbf2f
djm <dengjianmin12@outlook>
2022-06-18 22:45:04 +0800

    upgrade 2 we can change one data in  arraylist  with same type~

--- learn_numpy.py
+++ learn_numpy.py
@@ -4,14 +4,19 @@ import numpy as np
 from pytest import yield_fixture
 v = np.array(list(range(1,11))) #made a mistake cause someone named the file numpy.py...
 m = np.array([list(range(1,11)),list(range(10,0,-1))])
-print(v)
-print("------------------------------------------------")
+# print(v)
+# print("------------------------------------------------")
+# print(m)
+# print("------------------------------------------------")
+# print(type(v))
+# print("------------------------------------------------")
+# print(m.shape) #显示数据的行数，列数
+# print(np.shape(v)) 
+# print("------------------------------------------------")
+# print(m.size)
+# print(np.size(v))
+# print("------------------------------------------------")
+# print(v.dtype) #显示数据类型
+m[0,0] = 1900 #同类型数据，可替换
+'''m[0,0]="my name is ??" 此时由于数据类型不一致出现报错'''
 print(m)
-print("------------------------------------------------")
-print(type(v))
-print("------------------------------------------------")
-print(m.shape) #显示数据的行数，列数
-print(np.shape(v)) 
-print("------------------------------------------------")
-print(m.size)
-print(np.size(v))
