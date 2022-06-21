8c9f32dbb11959d281c6c094b0d30349b0b16664
djm <dengjianmin12@outlook>
2022-06-21 18:23:54 +0800

    upgrade file to show how far form the (0,0) and show how many times you need to call taxi to go home

--- random_test.py
+++ random_test.py
@@ -1,7 +1,7 @@
 import random
 from re import X
 
-from more_itertools import random_combination
+from more_itertools import one, random_combination
 
 def random_walk_V2(n):
     x,y = 0,0
@@ -11,9 +11,19 @@ def random_walk_V2(n):
         y += dy
     return (x,y)
 
-for i in range(10):
-    one_walk = random_walk_V2(100)
-    print(one_walk)
+walk_times = 50
+walk_step = 100
+how_far = 0
+call_taxi_times = 0
+for i in range(walk_times):
+    one_walk = random_walk_V2(walk_step)
+    how_far = abs(one_walk[0])+abs(one_walk[1])
+    if (how_far >= 20):
+        print("you will need a texi to go back home!\nwhere you are is:{}".format(one_walk))
+        call_taxi_times += 1
+    else:
+        print("you can go home by your foot!\nwhere you are is:{}".format(one_walk))
+print("the times of you call taxi to go home is:{}".format(call_taxi_times))
 
 
 # def random_walk(n):
