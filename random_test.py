883508d5d201e74e2f9008eaa0b12afa5c4deabe
djm <dengjianmin12@outlook>
2022-06-21 18:56:51 +0800

    upgrade file to show you a fool man who become to be a dog though playing game

--- random_test.py
+++ random_test.py
@@ -1,29 +1,47 @@
 import random
-from re import X
 
-from more_itertools import one, random_combination
 
-def random_walk_V2(n):
-    x,y = 0,0
-    for i in range(n):
-        dx,dy = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
-        x += dx
-        y += dy
-    return (x,y)
-
-walk_times = 50
-walk_step = 100
-how_far = 0
-call_taxi_times = 0
-for i in range(walk_times):
-    one_walk = random_walk_V2(walk_step)
-    how_far = abs(one_walk[0])+abs(one_walk[1])
-    if (how_far >= 20):
-        print("you will need a texi to go back home!\nwhere you are is:{}".format(one_walk))
-        call_taxi_times += 1
+#开赌开赌,梭哈加卡^-^~~
+def give_an_number():
+    number = random.randint(1,100)
+    print("-"*50)
+    if number >=50:
+        print("you won the game!~~\nthe number is:{}\n\
+play more to be a rich man~~".format(number))
+        return True
     else:
-        print("you can go home by your foot!\nwhere you are is:{}".format(one_walk))
-print("the times of you call taxi to go home is:{}".format(call_taxi_times))
+        print("you lost the game!\nthe number is:{}".format(number))
+        return False
+
+x = 0
+while(x < 100):
+    result = give_an_number()
+    print(result)
+    x += 1    
+
+# from more_itertools import one, random_combination
+
+# def random_walk_V2(n):
+#     x,y = 0,0
+#     for i in range(n):
+#         dx,dy = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
+#         x += dx
+#         y += dy
+#     return (x,y)
+
+# walk_times = 50
+# walk_step = 100
+# how_far = 0
+# call_taxi_times = 0
+# for i in range(walk_times):
+#     one_walk = random_walk_V2(walk_step)
+#     how_far = abs(one_walk[0])+abs(one_walk[1])
+#     if (how_far >= 20):
+#         print("you will need a texi to go back home!\nwhere you are is:{}".format(one_walk))
+#         call_taxi_times += 1
+#     else:
+#         print("you can go home by your foot!\nwhere you are is:{}".format(one_walk))
+# print("the times of you call taxi to go home is:{}".format(call_taxi_times))
 
 
 # def random_walk(n):
